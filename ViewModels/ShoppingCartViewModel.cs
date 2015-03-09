9685327df24e7fb9dbca547029d1a56f726a17e0
zulldesign <admin@zulldesign.ml>
2015-03-09 18:16:18 +0800

    l

---
+++ ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Potret.Models;
+
+namespace Potret.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
